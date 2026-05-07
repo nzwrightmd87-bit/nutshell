3988c1d33ace9a0ea3ed2a6f8fad5a389980607c
dissident5678 <252275868+dissident5678@users.noreply.github.com>
2026-05-06 23:25:41 -0400

    Allow collection counter backfill migration

--- db/migrate/20260507031900_backfill_collection_item_counts.rb
+++ db/migrate/20260507031900_backfill_collection_item_counts.rb
@@ -5,28 +5,36 @@ class BackfillCollectionItemCounts < ActiveRecord::Migration[8.0]
 
   def up
     say_with_time 'Backfilling collections.item_count from collection_items' do
-      execute <<~SQL.squish
-        UPDATE collections
-           SET item_count = collection_item_counts.item_count
-          FROM (
-            SELECT collection_id, COUNT(*) AS item_count
-              FROM collection_items
-             GROUP BY collection_id
-          ) AS collection_item_counts
-         WHERE collections.id = collection_item_counts.collection_id
-           AND collections.item_count IS DISTINCT FROM collection_item_counts.item_count
-      SQL
+      safety_assured do
+        begin
+          execute "SET lock_timeout TO '5s'"
 
-      execute <<~SQL.squish
-        UPDATE collections
-           SET item_count = 0
-         WHERE collections.item_count IS DISTINCT FROM 0
-           AND NOT EXISTS (
-             SELECT 1
-               FROM collection_items
-              WHERE collection_items.collection_id = collections.id
-           )
-      SQL
+          execute <<~SQL.squish
+            UPDATE collections
+               SET item_count = collection_item_counts.item_count
+              FROM (
+                SELECT collection_id, COUNT(*) AS item_count
+                  FROM collection_items
+                 GROUP BY collection_id
+              ) AS collection_item_counts
+             WHERE collections.id = collection_item_counts.collection_id
+               AND collections.item_count IS DISTINCT FROM collection_item_counts.item_count
+          SQL
+
+          execute <<~SQL.squish
+            UPDATE collections
+               SET item_count = 0
+             WHERE collections.item_count IS DISTINCT FROM 0
+               AND NOT EXISTS (
+                 SELECT 1
+                   FROM collection_items
+                  WHERE collection_items.collection_id = collections.id
+               )
+          SQL
+        ensure
+          execute 'RESET lock_timeout'
+        end
+      end
     end
   end
 
